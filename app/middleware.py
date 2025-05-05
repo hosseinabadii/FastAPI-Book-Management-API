1b50d7465d2d4b636d2aa5e7bf30fbd6fb75ae9a
hosseinabadii <khosro.h2011@gmail.com>
2025-05-05 12:51:57 +0330

    add * to middleware.py to trust all hosts

--- app/middleware.py
+++ app/middleware.py
@@ -57,5 +57,5 @@ def register_middleware(app: FastAPI):
 
     app.add_middleware(
         TrustedHostMiddleware,
-        allowed_hosts=["localhost", "127.0.0.1", "test"],
+        allowed_hosts=["localhost", "127.0.0.1", "test", "*"],
     )
